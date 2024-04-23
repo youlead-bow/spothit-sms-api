53fa1a0aaa7d4bd143d8d77c9cc1a3e38dc1ff94
Laurent Francois <l.francois@youlead.fr>
2024-04-23 15:22:07 +0200

    spothit

--- src/Webhook/RequestParser.php
+++ src/Webhook/RequestParser.php
@@ -19,7 +19,7 @@ final class RequestParser extends AbstractRequestParser
 
     protected function doParse(Request $request, #[\SensitiveParameter] string $secret): ?SmsEvent
     {
-        $spothit = $request->request->all();
+        $spothit = $request->query->all();
         if (
             !isset($spothit['statut'])
             || !isset($spothit['id_message'])
@@ -28,14 +28,14 @@ final class RequestParser extends AbstractRequestParser
             throw new RejectWebhookException(406, 'Spothit est incorrect.');
         }
 
-        $name = match ($spothit['MessageStatus']) {
-            0 => null,                      // En attente
-            1 => SmsEvent::DELIVERED,       // Livré
-            2 => null,                      // Envoyé
-            3 => null,                      // En cours
-            4 => SmsEvent::FAILED,          // Echec
-            5 => SmsEvent::FAILED,          // Expiré
-            default => throw new RejectWebhookException(406, sprintf('Évènement non supporté "%s".', $spothit['MessageStatus'])),
+        $name = match ($spothit['statut']) {
+            '0' => null,                      // En attente
+            '1' => SmsEvent::DELIVERED,       // Livré
+            '2' => null,                      // Envoyé
+            '3' => null,                      // En cours
+            '4' => SmsEvent::FAILED,          // Echec
+            '5' => SmsEvent::FAILED,          // Expiré
+            default => throw new RejectWebhookException(406, sprintf('Évènement non supporté "%s".', $spothit['statut'])),
         };
         if (!$name) {
             return null;
